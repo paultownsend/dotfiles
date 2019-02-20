f87d56a209b387499cbb2bedb111796b87f005ed
Paul Townsend <pt307@cam.ac.uk>
2019-02-20 20:32:56 +0000

    zsh: zgen and emacs stuff

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -5,3 +5,14 @@ _is_callable gdircolors && eval $(gdircolors -b $HOME/.dircolors)
 
 alias br=brew
 alias brup='brew upgrade'
+
+# Emacs
+e() {
+    if _is_running Emacs; then
+        emacsclient -n $@
+    else
+        /Applications/Emacs.app/Contents/MacOS/Emacs $@ &
+    fi
+}
+
+_is_callable emacs && alias emacs='emacs -nw'
